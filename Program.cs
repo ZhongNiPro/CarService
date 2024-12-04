f999b761db12b8580295c1439275f02eeb3406b6
ZhongNi <zhongnipro@gmail.com>
2024-12-04 23:45:09 +0300

    garbage cleaning

--- Program.cs
+++ Program.cs
@@ -1,25 +1,4 @@
-﻿//using System;
-//using System.Threading.Tasks;
-//using System.Windows.Forms;
-
-//namespace CarService
-//{
-//    internal static class Program
-//    {
-//        [STAThread]
-//        static void Main()
-//        {
-//            Application.EnableVisualStyles();
-//            Application.SetCompatibleTextRenderingDefault(false);
-//            Form1 form = new Form1();
-//            CarService carService = new CarService(form);
-//            Task.Run(() => carService.Work());
-//            //form.UpdateButtonClicked += carService.Work;
-//            Application.Run(form);
-//        }
-//    }
-//}
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -36,7 +15,6 @@ namespace CarService
             Form1 form = new Form1();
             var carService = new CarService(form);
 
-            // Запуск асинхронной логики сервиса
             Task.Run(async () => await carService.WorkAsync());
 
             Application.Run(form);
